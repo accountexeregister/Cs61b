3ee03d03745174d9a83d4f2ba4cd3ad5ebce8538
accountexeregister <yeohchongyi2@outlook.com>
2022-12-04 18:44:06 +0800

    Fix BuggyAList and complete random tests implementation

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -2,11 +2,59 @@ package randomizedtest;
 
 import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by hug.
  */
 public class TestBuggyAList {
-  // YOUR TESTS HERE
+    // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> correctList = new AListNoResizing<>();
+        BuggyAList<Integer> buggyAList = new BuggyAList<>();
+        for (int i = 0; i < 3; i++) {
+            correctList.addLast(i);
+            buggyAList.addLast(i);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            assertEquals(correctList.removeLast(), buggyAList.removeLast());
+        }
+    }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> buggyL = new BuggyAList<>();
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                buggyL.addLast(randVal);
+            } else if (operationNumber == 1) {
+                // size
+                int lSize = L.size();
+                int buggyLSize = buggyL.size();
+                assertEquals(lSize, buggyLSize);
+            } else {
+                if (L.size() == 0 || buggyL.size() == 0) {
+                    continue;
+                }
+                if (operationNumber == 2) {
+                    int lLast = L.getLast();
+                    int buggyLLast = buggyL.getLast();
+                    assertEquals(buggyLLast, lLast);
+                } else {
+                    int lLastRemoved = L.removeLast();
+                    int buggyLLastRemoved = buggyL.removeLast();
+                    assertEquals(lLastRemoved, buggyLLastRemoved);
+                }
+            }
+        }
+    }
 }
